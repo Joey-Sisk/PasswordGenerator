d7ff1189c8879f57d9d2dd4bafb5e90d426497ae
Joey Sisk <jjmsisk@gmail.com>
2020-10-27 21:39:02 -0500

    cleaned up repetition and unnecesary parts of code, added more detailed notes

--- assets/script.js
+++ assets/script.js
@@ -1,89 +1,64 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// sets the query selector in order to add functionality to the button on the page
+const generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// these are the base character options the user can choose from
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const integer = "1234567890";
+const symbol = "!@#$%^&*-_=+;:,.?`~";
 
-let passwordArr = [];
+// the generatePassword function will access and alter these variables
+let combinedCharacterChoices = ""; // this is where the character types are added to be accessed when creating the password
+let passwordTempHolder = ""; // the password is temporarily held in this while the last for loop runs
+let passwordLength; // the length of the password chosen by the user. Limits the last for loop in order to set length of the string
 
-// this object holds every possible character that the user can include in their password
-const characters = {
-  "lowerStr": "abcdefghijklmnopqrstuvwxyz",
-  "upperStr": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  "integerStr": "1234567890",
-  "symbolStr": "!@#$%^&*-_=+;:,.?`~"
-};
-
-// this array will gather the chosen character string to be called on when the password is assembled
-let charChoices = "";
-
-let passChar = "";
-
-let passwordTempHolder = ""
-
-// this allows for calling the different parts of the characters object
-const lower = characters.lowerStr;
-const upper = characters.upperStr;
-const integer = characters.integerStr;
-const symbol = characters.symbolStr;
+// these are only used to add character types and verify at least 1 type is selected
+let lowerCharactersAdd;
+let upperCharactersAdd;
+let integerCharactersAdd;
+let symbolCharactersAdd;
 
 // this is the default function provided to print the password out for the user
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
+  return;
 }
-
 
 // this function does all the work asking the user what to include and eventually assembling the password
 function generatePassword() {
 
-  // thus asks the user how long to make the password and ensures it meets the criteria
-  let passLength = prompt("Please choose a password length between 8 and 128.");
-    let passLengthInt = parseInt(passLength);
-    if (passLengthInt < 8 || passLengthInt > 128 || typeof passLengthInt != "number" || passLength != passLengthInt ) {
-    alert("Please use a whole number between 8 and 128");
+  // this asks the user how long to make the password and ensures it meets the criteria, it also converts the input to numericals and rounds down to the nearest whole number
+  passwordLength = Math.floor(parseInt(prompt("Please choose a password length between 8 and 128.")));
+    if (passwordLength < 8 || passwordLength > 128 || !passwordLength) {
+    alert("Please use a numerical between 8 and 128");
     return;
     }
 
-  // this asks the user what characters to include in the password
-  let lowerStrTrue = confirm("Would you like to use lowercase letters?");
-  let upperStrTrue = confirm("Would you like to use uppercase letters?");
-  let integerStrTrue = confirm("Would you like to use numbers?");
-  let symbolsStrTrue = confirm("Would you like to use any special characters?");
+  // these ask the user what characters to include in the password
+  lowerCharactersAdd = confirm("Would you like to use lowercase letters?");
+  upperCharactersAdd = confirm("Would you like to use uppercase letters?");
+  integerCharactersAdd = confirm("Would you like to use numbers?");
+  symbolCharactersAdd = confirm("Would you like to use any special characters?");
   
-  // this checks that the user has selected at least 1 type of character and adds the characters to the charTypes array
-  if (lowerStrTrue === true) {
-    charChoices += lower;
-  }
-  if (upperStrTrue === true) {
-    charChoices += upper;
-  }
-  if (integerStrTrue === true) {
-    charChoices += integer;
-  }
-  if (symbolsStrTrue === true) {
-    charChoices += symbol;
-  }
-  if (lowerStrTrue === false && upperStrTrue === false && integerStrTrue === false && symbolsStrTrue === false) {
+  // this checks that the user has selected at least 1 type of character and adds the characters to the combinedCharacterChoices variable
+  if (lowerCharactersAdd) combinedCharacterChoices += lower;
+  if (upperCharactersAdd) combinedCharacterChoices += upper;
+  if (integerCharactersAdd) combinedCharacterChoices += integer;
+  if (symbolCharactersAdd) combinedCharacterChoices += symbol;
+  if (!lowerCharactersAdd && !upperCharactersAdd && !integerCharactersAdd && !symbolCharactersAdd) {
     alert("Please choose at least 1 character type for your password.");
     return;
   }
 
-  // this assembles the password from random characters in the charChoicesStr string
-  for (var i = 0; i < passLengthInt; i++) {
-
-    passChar = charChoices.charAt(Math.floor(Math.random() * charChoices.length));
-
-    passwordTempHolder += passChar;    
-    
+  // this assembles the password from random characters in the combinedCharacterChoicesStr string
+  for (var i = 0; i < passwordLength; i++) {
+    passwordTempHolder += combinedCharacterChoices.charAt(Math.floor(Math.random() * combinedCharacterChoices.length)); 
   }
-  
-return password = passwordTempHolder;
+
+return password = passwordTempHolder; // sends finished password to the writePassword function
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// charAt.charChoices(Math.floor(Math.random() * charChoices.length))
